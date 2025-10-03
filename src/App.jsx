8ada5cf11ff79c954d4170176ed68a86e61e577c
Kshitij <kshitijsharma1820@gmail.com>
2025-10-03 11:57:26 +0530

    updated HashRouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { ThemeProvider } from './context/ThemeContext'
 import Navigation from './components/Navigation'
